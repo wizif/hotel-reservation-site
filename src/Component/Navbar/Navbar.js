bff55ef4a31bf2a8915188f3a51183a6ad0fdf0f
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-06-18 22:51:06 +0530

    _

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
+  <nav className='=navbar'>
+    <div className="container flex_space">
+        <div className="meni-icon">
+            <i className="fas fa-times"></i>
+        </div>
     </div>
+<div className="nav-menu active">
+     
+</div>
+  </nav>
   )
 }
 
