194d47194b1d16bf40e2dd7cc35e954b4160a563
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-06-24 22:43:29 +0530

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './common/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Component/HomeSection/Pages/Home';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<div>Home Page</div>} />
+          <Route path='/' element={Home} />
         </Routes>
       </Router>
     </>
---
+++ src/Component/HomeSection/Data.js
@@ -0,0 +1,5 @@
+const Data=[
+    {
+        images:"images/slider-1.jpg"
+    }
+]
